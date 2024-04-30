4eb276ce9b2625d025a19932292dcfc12db6b1a0
Ns-Jin <sktkdwls1222@pusan.ac.kr>
2024-04-30 20:21:52 +0900

    main page overview (04.30)

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,10 @@
-let game_name = document.getElementById('12_chess');
-let preview_12_chess = document.getElementById('mouse');
+let menu_left = document.getElementById('menu_left');
+let menu_right = document.getElementById('menu_right');
 
-game_name.addEventListener("mouseover", (e) => {
-    let mouesX = game_name.offsetLeft + 155;
-    let mouesY = game_name.offsetTop + 15;
-    preview_12_chess.style.visibility = "visible";
-    preview_12_chess.style.left = mouesX + "px";
-    preview_12_chess.style.top = mouesY + "px";
-    console.log(mouesX,mouesY);
+menu_left.addEventListener("click", (e) => {
+    window.location.href = "./pages/agent_learning.html";
 });
 
-game_name.addEventListener("mouseout", (e) => {
-    preview_12_chess.style.visibility = "hidden";
+menu_right.addEventListener("click", (e) => {
+    window.location.href = "./pages/tc_battle.html";
 });
